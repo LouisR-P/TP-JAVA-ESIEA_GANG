da5342fa4991e60912511274cddbd0b4de8fd8ff
louis <louis.russopelosi@hotmail.fr>
2020-02-29 15:20:26 +0100

    Simplification du code avec la méthode ordinal() permettant de retrouver le numéro d'ordre d'un élément énuméré dans la liste de tous les éléments d'une énumération. Ainsi, le changement de direction ("r" et "l") est géré indépendamment du déplacement ("f" et "b").

--- src/main/java/com/esiea/tp4A/domain/Direction.java
+++ src/main/java/com/esiea/tp4A/domain/Direction.java
@@ -3,4 +3,25 @@ package com.esiea.tp4A.domain;
 public enum Direction {
 
     NORTH, EAST, SOUTH, WEST;
+
+    public Direction left() {
+        int leftOrdinal = ordinal()-1;
+        if (leftOrdinal == -1){
+            return WEST;
+        }
+        else {
+            return Direction.values()[leftOrdinal];
+        }
+    }
+
+    public Direction right() {
+        int rightOrdinal = ordinal()+1;
+        if (rightOrdinal == 3){
+            return NORTH;
+        }
+        else {
+            return Direction.values()[rightOrdinal];
+        }
+    }
+
 }
