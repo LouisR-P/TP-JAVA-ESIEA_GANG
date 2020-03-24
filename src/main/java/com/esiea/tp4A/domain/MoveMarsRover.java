e2f5863a7eb33fe37c13f0e50c10043c619be770
louis <louis.russopelosi@hotmail.fr>
2020-03-24 15:31:20 +0100

    Modification du code concernant les problèmes soulignés dans l'issue suivante (https://github.com/LouisR-P/TP-JAVA-ESIEA_GANG/issues/8) : - Ajout des mot-clés private et final devant la variable de position; - Suppression de la prise en compte de préfixe/séparateur/suffixe pour la commande entrée en paramètre (pas de crochet ou de virgule à utiliser, seuls les caractères inconnus doivent être ignorés).

--- src/main/java/com/esiea/tp4A/domain/MoveMarsRover.java
+++ src/main/java/com/esiea/tp4A/domain/MoveMarsRover.java
@@ -2,7 +2,7 @@ package com.esiea.tp4A.domain;
 
 public class MoveMarsRover implements MarsRover {
 
-    Position position;
+    private final Position position;
 
     public MoveMarsRover(int x, int y, Direction direction) {
         position = Position.of(x, y, direction);
@@ -10,86 +10,40 @@ public class MoveMarsRover implements MarsRover {
 
     @Override
     public Position move(String command) {
-
         int xPosition = position.getX();
         int yPosition = position.getY();
         Direction direction = position.getDirection();
 
-        position = Position.of(xPosition, yPosition, direction);
+        command = command.toLowerCase(); // Sensibilité à la casse
 
-
-        if (command.startsWith("[") && command.endsWith("]")){
-            // Suppression du préfixe et du suffixe de la chaîne de caractères saisie en entrée
-            command = command.substring(1);
-            command = command.substring(0,command.length()-1);
-            // Suppression des espaces de la chaîne de caractères saisie en entrée
-            command = command.replaceAll("\\s", "");
-        } else {
-            throw new InvalidCommandException("Commande " + command + " inconnue");
-        }
-
-        // Définition du séparateur et séparation des instructions en une unique instruction
-        final String separateur = ",";
-        String instructions[] = command.split(separateur);
-
-
-        for (int i = 0; i < instructions.length; i++) {
-
-            instructions[i] = instructions[i].toLowerCase();
-
-            if (instructions[i].equals("l")) {
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'l') {
                 direction = direction.left();
             }
-
-            else if (instructions[i].equals("r")) {
+            else if (command.charAt(i) == 'r') {
                 direction = direction.right();
             }
-
-            // CONDIITON A AJOUTER  || command.startsWith("[") && command.endsWith("]"))
-            else if (instructions[i].equals("f") || instructions[i].equals("b")) {
-
+            else if ((command.charAt(i) == 'f') || (command.charAt(i) == 'b')) {
                 switch (direction) {
-
                     case NORTH:
-                        if (instructions[i].equals("f")) {
-                            yPosition = yPosition + 1;
-                        } else if (instructions[i].equals("b")) {
-                            yPosition = yPosition - 1;
-                        } break;
-
+                        yPosition = (command.charAt(i) == 'f') ? yPosition + 1 : yPosition - 1;
+                        break;
                     case EAST:
-                        if (instructions[i].equals("f")) {
-                            xPosition = xPosition + 1;
-                        } else if (instructions[i].equals("b")) {
-                            xPosition = xPosition - 1;
-                        } break;
-
+                        xPosition = (command.charAt(i) == 'f') ? xPosition + 1 : xPosition - 1;
+                        break;
                     case SOUTH:
-                        if (instructions[i].equals("f")) {
-                            yPosition = yPosition - 1;
-                        } else if (instructions[i].equals("b")) {
-                            yPosition = yPosition + 1;
-                        } break;
-
+                        yPosition = (command.charAt(i) == 'f') ? yPosition - 1 : yPosition + 1;
+                        break;
                     case WEST:
-                        if (instructions[i].equals("f")) {
-                            xPosition = xPosition - 1;
-                        } else if (instructions[i].equals("b")) {
-                            xPosition = xPosition + 1;
-                        } break;
-
+                        xPosition = (command.charAt(i) == 'f') ? xPosition - 1 : xPosition + 1;
+                        break;
                 }
             }
-
             else {
                 throw new InvalidCommandException("Commande " + command + " inconnue");
             }
-
         }
-
-        position = Position.of(xPosition, yPosition, direction);
-        Position.FixedPosition fixedPosition = new Position.FixedPosition(position.getX(), position.getY(), position.getDirection());
+        Position.FixedPosition fixedPosition = new Position.FixedPosition(xPosition, yPosition, direction);
         return fixedPosition;
-
     }
 }
--- src/test/java/com/esiea/tp4A/domain/MoveMarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MoveMarsRoverTest.java
@@ -5,37 +5,51 @@ import org.junit.jupiter.api.Test;
 
 class MoveMarsRoverTest {
     @Test
+    void notMoving() throws Exception {
+        MarsRover marsRover = new MoveMarsRover(0,0, Direction.NORTH);
+        Position newPosition = marsRover.move("");
+        Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 0, Direction.NORTH));
+    }
+
+    @Test
     void MovingForwardRover() throws Exception {
         MarsRover marsRover = new MoveMarsRover(0, 0, Direction.NORTH);
-        Position newPosition = marsRover.move("[f]");
+        Position newPosition = marsRover.move("f");
         Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 1, Direction.NORTH));
     }
 
     @Test
     void MovingBackwardRover() throws Exception {
         MarsRover marsRover = new MoveMarsRover(0, 0, Direction.NORTH);
-        Position newPosition = marsRover.move("[b]");
+        Position newPosition = marsRover.move("b");
         Assertions.assertThat(newPosition).isEqualTo(Position.of(0, -1, Direction.NORTH));
     }
 
     @Test
     void TurningRightRover() throws Exception {
         MarsRover marsRover = new MoveMarsRover(0, 0, Direction.NORTH);
-        Position newPosition = marsRover.move("[r]");
+        Position newPosition = marsRover.move("r");
         Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 0, Direction.EAST));
     }
 
     @Test
     void TurningLeftRover() throws Exception {
         MarsRover marsRover = new MoveMarsRover(0, 0, Direction.NORTH);
-        Position newPosition = marsRover.move("[l]");
+        Position newPosition = marsRover.move("l");
         Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 0, Direction.WEST));
+    }
+
+    @Test
+    void complexMovesFromDifferentOrigin() throws Exception {
+        MarsRover marsRover = new MoveMarsRover(4, 5, Direction.WEST);
+        Position newPosition = marsRover.move("ff");
+        Assertions.assertThat(newPosition).isEqualTo(Position.of(2, 5, Direction.WEST));
     }
 
     @Test
     void MajCommand() throws Exception {
         MarsRover marsRover = new MoveMarsRover(0, 0, Direction.NORTH);
-        Position newPosition = marsRover.move("[F]");
+        Position newPosition = marsRover.move("F");
         Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 1, Direction.NORTH));
     }
 
@@ -43,14 +57,14 @@ class MoveMarsRoverTest {
     void WrongCommand() {
         MarsRover marsRover = new MoveMarsRover(0, 0, Direction.NORTH);
         Assertions.assertThatExceptionOfType(InvalidCommandException.class)
-            .isThrownBy(() -> marsRover.move("[o]"));
+            .isThrownBy(() -> marsRover.move("o"));
     }
-
 
     @Test
     void SuccessionMovingRover() throws Exception {
         MarsRover marsRover = new MoveMarsRover(0, 0, Direction.NORTH);
-        Position newPosition = marsRover.move("[f, f, l, b]");
+        Position newPosition = marsRover.move("fflb");
         Assertions.assertThat(newPosition).isEqualTo(Position.of(1, 2, Direction.WEST));
     }
+
 }
