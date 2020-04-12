0672725ac6a0d243ab24d6f75b3a2d0e234f8f1c
taharbenoudjit <benoudjit.esiea.fr>
2020-04-12 20:24:00 +0200

    Création package et conception d'une API à l'aide d'une nouvelle interface.

---
+++ src/test/java/com/esiea/tp4AOlineGame/Game.java
@@ -0,0 +1,24 @@
+package com.esiea.tp4AOlineGame;
+
+import com.esiea.tp4A.domain.Position;
+
+import java.util.Set;
+
+public interface Game {
+
+    // Connaître la position de son Rover
+    Position roverPosition(String playerName);
+
+    // Connaître la position des obstacles et joueurs adverses dans un carré 30x30 autour du joueur
+    Set<Position> radar(String playerName);
+
+    // Connaître la portée du laser
+    int laserRange();
+
+    // se déplacer (+ tirer avec son laser -> command "s")
+    Position move(String command);
+
+    // Connaître le statut de son personnage (mort ou vivant)
+    Boolean alive();
+
+}
