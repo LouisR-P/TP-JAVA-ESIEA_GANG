2c80dcb310066e48a09de0de4456955f0df78dec
Georges <Georges@DESKTOP-C29I1NU.(none)>
2020-04-12 23:30:30 +0200

    Ajout d'un test de l'API.

--- src/test/java/com/esiea/tp4AOnlineGame/PlayerControllerTest.java
+++ src/test/java/com/esiea/tp4AOnlineGame/PlayerControllerTest.java
@@ -3,6 +3,7 @@ package com.esiea.tp4AOnlineGame;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PlayerControllerTest {
@@ -14,6 +15,15 @@ public class PlayerControllerTest {
         game.addPlayer("Name");
         player = player.startNew(game, "Name");
         // coder la fonction addPlayer puis Assertions
+    }
+
+    @Test
+    void roverPostionOfThePlayer(){
+        PlayerController player = new PlayerControllerImpl(null, null);
+        Game game = new GameImpl();
+        game.addPlayer("Name");
+        player = player.startNew(game,"Name");
+        assertEquals(game.roverPosition("Name"), player.roverPosition());
     }
 
 }
